b953fdcf58cb7c5cfbd648c2fe50279ff86a882c
nzai <nzaih18@gmail.com>
2023-04-18 11:13:01 +0300

    rr

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
 const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(count);
+
   return (
     <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => setCount(count - 1)}>➖ Decrement</button>
+        <button onClick={() => setCount(0)}>🔁 Reset</button>
+        <button onClick={() => setCount(count + 1)}> ➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setInput(input);
+          }}
+        >
+          <input
+            type="number"
+            value={input}
+            onChange={(e) => setCount(+e.target.value)}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
